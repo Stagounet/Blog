79ed4c3e95af23ea384a8dd64198b36c38c87884
Stagounet <piro@live.be>
2013-11-07 22:01:06 +0100

    Add / to hostname

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://piro.azurewebsites.net',
+        url: 'http://piro.azurewebsites.net/',
         mail: {},
         database: {
             client: 'sqlite3',
